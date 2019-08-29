9186c0c4e0a3c350da098b7a8a3432adf93a4eca
Piotr Sadowski <piotrsadowski09@gmail.com>
2019-08-29 18:50:44 +0200

    9

--- src/comp/Contact.js
+++ src/comp/Contact.js
@@ -32,13 +32,15 @@ class Contact extends React.Component {
     render() {
         return (
             <div id="contact" className={"contactUs_box"}>
-                <p className={"contactUs"}>Skontaktuj sie z nami </p>
-                <div className={"decor"}></div>
-                <p className={"contactUs_info"}>Formularz kontaktowy</p>
-                <input className={"contactUs_input"} type="text" placeholder="Imie" value={this.state.name} onChange={this.onNameChange}/>
-                <input className={"contactUs_input"} type="email" placeholder="E-mail" value={this.state.email} onChange={this.onEmailChange}/>
-                <input className={"contactUs_input"} type="text" placeholder="Wiadomosc" value={this.state.Message} onChange={this.MessageChange}/>
-                <button onSubmit={this.onFormSubmit}>Wyslij</button>
+                <div className={"contactUs_boxtwo"}>
+                    <p className={"contactUs"}>Skontaktuj sie z nami </p>
+                    <div className={"decor"}></div>
+                    <p className={"contactUs_info"}>Formularz kontaktowy</p>
+                    <input className={"contactUs_input"} type="text" placeholder="Imie" value={this.state.name} onChange={this.onNameChange}/>
+                    <input className={"contactUs_input"} type="email" placeholder="E-mail" value={this.state.email} onChange={this.onEmailChange}/>
+                    <input className={"contactUs_input"} type="text" placeholder="Wiadomosc" value={this.state.Message} onChange={this.MessageChange}/>
+                    <button onSubmit={this.onFormSubmit}>Wyslij</button>
+                </div>
                
             </div>
         )
